bd5553173efd0a4120ed01c1d60d8a7b0ff7c912
AmirHosseinSadeghi <amirhosseinsadeghy1380@gmail.com>
2022-04-12 02:23:28 +0430

    Make HandyPatternbtn Code & BFS algoritm

--- ProjectHosh/MainWindow.xaml.cs
+++ ProjectHosh/MainWindow.xaml.cs
@@ -22,15 +22,25 @@ namespace ProjectHosh
     {
         #region LocalVariable
         Label[,] Labels;
+        int[] StartPosition;
+        bool Success;
+        bool HandyPattern;
+        bool SelectRed;
+        bool SelectGreen;
         #endregion
         public MainWindow()
         {
             InitializeComponent();
             Labels = new Label[20, 30];
+            StartPosition = new int[2];
+            Success = false;
+            HandyPattern = false;
+            SelectRed = true;
+            SelectGreen= true;
             MakeMap();
         }
 
-        public void MakeMap()
+        private void MakeMap()
         {
             #region ColumnDefinitions
             Map.ColumnDefinitions.Add(new ColumnDefinition());
@@ -94,6 +104,7 @@ namespace ProjectHosh
                 {
                     Label lb = new Label();
                     lb.BorderThickness = new Thickness(1);
+                    lb.MouseLeftButtonUp += Lb_MouseLeftButtonUp;
                     if (i == 0 || i == 19 || j == 0 || j == 29)
                         lb.Background = new SolidColorBrush(Colors.Black);
                     else
@@ -106,14 +117,46 @@ namespace ProjectHosh
             }
         }
 
+        private void Lb_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (HandyPattern)
+            {
+                var temp = (Label)sender;
+                var backgroundColor = temp.Background;
+                var color = ((SolidColorBrush)backgroundColor).Color.ToString();
+                if (color == new SolidColorBrush(Colors.White).ToString())
+                {
+                    if (SelectRed)
+                    {
+                        temp.Background = new SolidColorBrush(Colors.Red);
+                        SelectRed = false;
+                    }
+                    else if (SelectGreen)
+                    {
+                        temp.Background = new SolidColorBrush(Colors.Green);
+                        SelectGreen = false;
+                    }
+                    else
+                    {
+                        temp.Background = new SolidColorBrush(Colors.Black);
+                    }
+                }
+            }
+        }
+
         private void GeneratePatternbtn_Click(object sender, RoutedEventArgs e)
         {
+            HandyPattern = false;
+            SelectRed = true;
+            SelectGreen = true;
             Clear();
 
             Random random = new Random();
             int i = random.Next(1, 19);
             int j = random.Next(1, 29);
-            Labels[i, j].Background = new SolidColorBrush(Colors.Green);
+            Labels[i, j].Background = new SolidColorBrush(Colors.Red);
+            StartPosition[0] = i;
+            StartPosition[1] = j;
 
             while (true)
             {
@@ -123,13 +166,13 @@ namespace ProjectHosh
                 var color = ((SolidColorBrush)backgroundColor).Color.ToString();
                 if (color == new SolidColorBrush(Colors.White).ToString())
                 {
-                    Labels[i, j].Background = new SolidColorBrush(Colors.Red);
+                    Labels[i, j].Background = new SolidColorBrush(Colors.Green);
                     break;
                 }
 
             }
 
-            int t = random.Next(0, 503);// 600-30-30-18-18-2 = 502
+            int t = random.Next(0, 350);// 600-30-30-18-18-2 = 502
             while (t >= 0)
             {
                 i = random.Next(1, 19);
@@ -143,7 +186,8 @@ namespace ProjectHosh
                 }
             }
         }
-        public void Clear()
+
+        private void Clear()
         {
             for (int i = 1; i < 19; i++)
             {
@@ -157,9 +201,145 @@ namespace ProjectHosh
             }
         }
 
+        private void BFS_Algoritm()
+        {
+            Queue<Label> waitingQueue = new Queue<Label>();
+            Queue<Label> visitedQueue = new Queue<Label>();
+            int i = StartPosition[0];
+            int j = StartPosition[1];
+            visitedQueue.Enqueue(Labels[i, j]);
+            var backgroundColor = Labels[i - 1, j].Background;
+            var color = ((SolidColorBrush)backgroundColor).Color.ToString();
+            do
+            {
+                // Top
+                if (color == new SolidColorBrush(Colors.White).ToString())
+                {
+                    waitingQueue.Enqueue(Labels[i - 1, j]);
+                    Labels[i - 1, j].Background = new SolidColorBrush(Colors.LightPink);
+                    Labels[i - 1, j].Content = $"{i - 1},{j},{i},{j}";
+                }
+                else if (color == new SolidColorBrush(Colors.Green).ToString())
+                {
+                    visitedQueue.Enqueue(Labels[i, j]);
+                    Success = true;
+                    break;
+                }
+
+                // Right
+                backgroundColor = Labels[i, j + 1].Background;
+                color = ((SolidColorBrush)backgroundColor).Color.ToString();
+                if (color == new SolidColorBrush(Colors.White).ToString())
+                {
+                    waitingQueue.Enqueue(Labels[i, j + 1]);
+                    Labels[i, j + 1].Background = new SolidColorBrush(Colors.LightPink);
+                    Labels[i, j + 1].Content = $"{i},{j + 1},{i},{j}";
+                }
+                else if (color == new SolidColorBrush(Colors.Green).ToString())
+                {
+                    visitedQueue.Enqueue(Labels[i, j]);
+                    Success = true;
+                    break;
+                }
+
+                // Bottom
+                backgroundColor = Labels[i + 1, j].Background;
+                color = ((SolidColorBrush)backgroundColor).Color.ToString();
+                if (color == new SolidColorBrush(Colors.White).ToString())
+                {
+                    waitingQueue.Enqueue(Labels[i + 1, j]);
+                    Labels[i + 1, j].Background = new SolidColorBrush(Colors.LightPink);
+                    Labels[i + 1, j].Content = $"{i + 1},{j},{i},{j}";
+                }
+                else if (color == new SolidColorBrush(Colors.Green).ToString())
+                {
+                    visitedQueue.Enqueue(Labels[i, j]);
+                    Success = true;
+                    break;
+                }
+
+                // Left
+                backgroundColor = Labels[i, j - 1].Background;
+                color = ((SolidColorBrush)backgroundColor).Color.ToString();
+                if (color == new SolidColorBrush(Colors.White).ToString())
+                {
+                    waitingQueue.Enqueue(Labels[i, j - 1]);
+                    Labels[i, j - 1].Background = new SolidColorBrush(Colors.LightPink);
+                    Labels[i, j - 1].Content = $"{i},{j - 1},{i},{j}";
+                }
+                else if (color == new SolidColorBrush(Colors.Green).ToString())
+                {
+                    visitedQueue.Enqueue(Labels[i, j]);
+                    Success = true;
+                    break;
+                }
+
+                var temp = waitingQueue.Dequeue(); 
+                while (true)
+                {
+                    if (visitedQueue.Contains(temp))
+                        temp = waitingQueue.Dequeue();
+                    else
+                        break;
+                }
+                visitedQueue.Enqueue(temp);
+                var currentPosition = temp.Content.ToString();
+                var x = currentPosition.Split(',');
+                i = Convert.ToInt32(x[0]);
+                j = Convert.ToInt32(x[1]);
+            } while (waitingQueue.Count > 0);
+
+            if (Success)
+            {
+                int counter = 1;
+                var currentPosition = visitedQueue.Dequeue().Content.ToString();
+                var x = currentPosition.Split(',');
+                while (true)
+                {
+                    i = Convert.ToInt32(x[2]);
+                    j = Convert.ToInt32(x[3]);
+                    if (i == StartPosition[0]&& j == StartPosition[1])
+                        break;
+                    Labels[i, j].Background = new SolidColorBrush(Colors.LightBlue);
+                    Labels[i, j].Content = counter;
+                    counter++;
+                    currentPosition = Labels[i,j].Content.ToString();
+                    x = currentPosition.Split(',');
+                }
+            }
+
+            foreach(Label lb in Labels)
+            {
+                backgroundColor = lb.Background;
+                color = ((SolidColorBrush)backgroundColor).Color.ToString();
+                if (color == new SolidColorBrush(Colors.LightPink).ToString())
+                {
+                    lb.Content = null;
+                }
+            }
+        }
+
         private void Clearbtn_Click(object sender, RoutedEventArgs e)
         {
+            HandyPattern = false;
+            SelectRed = true;
+            SelectGreen = true;
             Clear();
+        }
+
+        private void Searchbtn_Click(object sender, RoutedEventArgs e)
+        {
+            HandyPattern = false;
+            SelectRed = true;
+            SelectGreen = true;
+
+            if (combobox.SelectedIndex==0)
+                BFS_Algoritm();
+        }
+
+        private void HandyPatternbtn_Click(object sender, RoutedEventArgs e)
+        {
+            HandyPattern = true;
         }
     }
 }
